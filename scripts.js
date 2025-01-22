1159828cb8f5d92cd820decfc539f7e802381d6d
Montserrad Fallas Ruiz <montserradfallas@gmail.com>
2025-01-22 14:51:56 -0500

    Implemented word-small.json file for new, larger word bank

--- scripts.js
+++ scripts.js
@@ -1,16 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetchWords();
+});
+
+function fetchWords(){
+    fetch("words-small.json")
+        .then((response) => {
+            if (!response.ok){
+                throw new Error("Failed to fetch JSON file");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            populateWordBank(data.words);
+        });
+}
+
+function populateWordBank(words) {
+    const wordBank = document.getElementById("word_bank");
+    wordBank.innerHTML = "";
+
+    words.forEach((word) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.textContent = word;
+        li.appendChild(a);
+        wordBank.appendChild(li);
+    });
+}
+
 function wordFinder(){
     var input = document.getElementById("user_input");
     var new_word = input.value.toLowerCase();
     var word_bank = document.getElementById("word_bank");
     var word = word_bank.getElementsByTagName("li");
-    var found = false;
 
-    for(index = 0; index < word.length; index++){
-        a = word[index].getElementsByTagName("a")[0];
+    for(var index = 0; index < word.length; index++){
+        var a = word[index].getElementsByTagName("a")[0];
         var value = a.textContent || a.innerText;
         if(value.toLowerCase().indexOf(new_word) > -1){
             word[index].style.display="";
-            found = true;
         }
         else{
             word[index].style.display = "none";
